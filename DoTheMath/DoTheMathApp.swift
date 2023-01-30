4d84ac86f786b390a024a95c7539226687b6fe7a
Melody Davis <piano.fan122@gmail.com>
2023-01-30 14:22:00 -0500

    Initial Commit

---
+++ DoTheMath/DoTheMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoTheMathApp.swift
+//  DoTheMath
+//
+//  Created by Melody Davis on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct DoTheMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
